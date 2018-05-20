c42abd373e12dee657eab81439d71b980626e264
James Twigg <james.rm.twigg@gmail.com>
2018-05-20 15:41:49 -0700

    Initial commit. Map implements get, put, and erase, but does not actually evict old values yet.

---
+++ DecayingMap.cpp
@@ -0,0 +1,116 @@
+#include <map>
+#include <chrono>
+#include <memory>
+
+using namespace std;
+using namespace std::chrono;
+
+template <class K, class V> class DecayingPointer {
+	milliseconds ttd;
+	K key;
+	shared_ptr<V> ptr;
+
+	DecayingPointer(K k, V* v, milliseconds ttd) : key(k), ttd(ttd) {
+		ptr = shared_ptr<V>(v);
+	}
+};
+
+/**
+ * A map where an element is removed if it is not accessed within a configurable
+ * timeout (default 5s). If this map had the last reference to that object then
+ * it is destroyed at the time of removal.
+ */
+template <class K, class V>
+class DecayingMap {
+/**
+ * TODO
+ * Launch Janitor thread which deletes expired elements.
+ * How to schedule the janitor thread to wake up when an element decays.
+ * How to REschedule the janitor when the next item to decay is refreshed.
+ */
+
+private:
+	//Future project: a generic (templated) map with two keys. Or an arbitrary number?
+	map<K,DecayingPointer<K,V>> dataByKey;
+	map<milliseconds, DecayingPointer<K,V>> dataByTtd;
+	milliseconds decayTime = milliseconds(5000);
+
+	/**
+	 * Finds the element of dataByTtd with the given key and ttd, if it exists.
+	 * Returns an iterator to that element if it exists, or an iterator to
+	 *   dataByTtd.end() if not.
+	 */
+	typename map<milliseconds,DecayingPointer<K,V>>::iterator findByMillisAndKey(K key, milliseconds ms) {
+		auto it = dataByTtd.find(ms);
+		if(it == dataByTtd.end()) {
+			return it;
+		}
+
+		for(; it != dataByTtd.end() && it->ttd == ms; ++it) {
+			if(it->key == key) return it;
+		}
+		return dataByTtd.end();
+	}
+
+	milliseconds nowPlusDecay() {
+		return duration_cast<milliseconds>(system_clock::now().time_since_epoch() + decayTime);
+	}
+
+public:
+	DecayingMap(long decayTime = 5000L) {
+		this->decayTime = milliseconds(decayTime);
+	}
+
+	/**
+	 * Inserts the given key and value into the map.
+	 * If there's already an entry with this key it is removed from the map,
+	 *   and if that is the last reference to the object it is deleted.
+	 * Returns a shared_ptr to the given object.
+	 */
+	shared_ptr<V> put(K key, V* value) {
+		erase(key);//does nothing if no key
+		milliseconds ttd = nowPlusDecay();
+		auto elem = DecayingPointer<K,V>(key, value, ttd);
+		dataByKey[key] = elem;
+		dataByTtd[ttd] = elem;
+		//force creation of new reference
+		return shared_ptr<V>(dataByKey[key].ptr);
+	}
+
+	/**
+	 * Erases from this map the element with key 'key', if there is one.
+	 * If so destructs the shared_ptr to that object and returns true.
+	 * Otherwise does nothing and returns false.
+	 */
+	bool erase(K key) {
+		auto keyItr = dataByKey.find(key);
+		if(keyItr == dataByKey.end()) {
+			return false;
+		}
+
+		auto ttdItr = findByMillisAndKey(keyItr->key, keyItr->ttd);
+		dataByKey.erase(keyItr);
+		if(ttdItr != dataByTtd.end()) { //should always be true.
+			dataByTtd.erase(ttdItr);
+		}
+		return true;
+	}
+
+	/**
+	 * Retrieves an element from the map, refreshing its decay time.
+	 * If there is no element in the map with this key returns an empty ptr.
+	 */
+	shared_ptr<V> get(K key) {
+		auto keyItr = dataByKey.find(key);
+		if(keyItr == dataByKey.end()) {
+			return shared_ptr<V>();
+		}
+		auto ttdItr = findByMillisAndKey(key, keyItr->ttd);
+		if(ttdItr != dataByTtd.end()) {
+			dataByTtd.erase(ttdItr);
+		}
+		keyItr->ttd = nowPlusDecay();
+		dataByTtd[keyItr->ttd] = *keyItr;
+		return shared_ptr<V>(keyItr->ptr);
+	}
+};
